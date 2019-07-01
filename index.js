537195f1e231531df508698f6ff8c339b311c26c
Paul Wiarda <wiarda@gmail.com>
2019-07-01 17:24:58 +0800

    -updated exports

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const FormButton = require('./components/FormButton');
 const FormCheckbox = require('./components/FormCheckbox');
 const FormFieldFile = require('./components/FormFieldFile');
 const FormSelectPretty = require('./components/FormSelectPretty');
-const FormStateWrapper = require('./components/FormStateWrapper');
+const FormStatusWrapper = require('./components/FormStatusWrapper');
 const IfResolved = require('./components/IfResolved');
 const IfFailed = require('./components/IfFailed');
 const IfSubmitting = require('./components/IfSubmitting');
 const IfActive = require('./components/IfActive');
 
-module.exports = { Form, FormButton, FormCheckbox, FormFieldFile, FormField, FormSelectPretty, FormStateWrapper, IfResolved, IfFailed, IfSubmitting, IfActive };
+module.exports = {
+  Form,
+  FormButton,
+  FormCheckbox,
+  FormFieldFile,
+  FormField,
+  FormSelectPretty,
+  FormStatusWrapper,
+  IfResolved,
+  IfFailed,
+  IfSubmitting,
+  IfActive,
+};
