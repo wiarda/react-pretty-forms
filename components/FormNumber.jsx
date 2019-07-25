dca2cb203d8c0cfd54e4f71a665167b137b12277
Paul Wiarda <wiarda@gmail.com>
2019-07-25 12:53:14 +0800

    -number type field with min / max

---
+++ components/FormNumber.jsx
@@ -0,0 +1,95 @@
+const React = require('react');
+const PropTypes = require('prop-types');
+const FormField = require('./FormField');
+const Label = require('./Label');
+
+class FormNumber extends FormField {
+  render() {
+    const {
+      name,
+      required = false,
+      initialValue,
+      styles = {},
+      className = styles.prettyInput || 'pretty-input',
+      rowClassName = styles.prettyRow || 'pretty-row',
+      validationClassName = styles.prettyValidation || 'pretty-validation',
+      labelTextClassName = styles.prettyLabelText || 'pretty-label-text',
+      labelClassName = styles.prettyLabel || 'pretty-label',
+      label = false,
+      min,
+      max,
+      validationMessage: exclude,
+      ...remaining
+    } = this.props;
+    const { validationMessage, isValid, labelUp } = this.state;
+
+    return (
+      <div
+        className={rowClassName}
+        {...remaining}
+      >
+        <Label
+          className={labelClassName}
+          labelTextClassName={labelTextClassName}
+          htmlFor={name}
+          label={label}
+          data-validity={isValid}
+          data-active={labelUp}
+        >
+          <input
+            className={className}
+            required={required}
+            type='number'
+            min={min}
+            max={max}
+            name={name}
+            ref={this.inputRef}
+            defaultValue={initialValue}
+            onBlur={this.blurHandler}
+            onFocus={this.focusHandler}
+            data-validity={isValid}
+          />
+        </Label>
+        <div
+          className={validationClassName}
+          data-validity={isValid}
+        >
+          {validationMessage}
+        </div>
+      </div>
+    );
+  }
+}
+
+FormNumber.propTypes = {
+  name: PropTypes.string.isRequired,
+  initialValue: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+  required: PropTypes.bool,
+  className: PropTypes.string,
+  rowClassName: PropTypes.string,
+  validationClassName: PropTypes.string,
+  labelClassName: PropTypes.string,
+  labelTextClassName: PropTypes.string,
+  validator: PropTypes.func,
+  validationMessage: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+};
+
+FormNumber.defaultProps = {
+  required: false,
+  label: false,
+  type: 'text',
+};
+
+FormNumber._prettyType = 'Field';
+
+module.exports = FormNumber;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const FormButton = require('./components/FormButton');
 const FormCheckbox = require('./components/FormCheckbox');
 const FormFile = require('./components/FormFile');
 const FormSelect = require('./components/FormSelect');
+const FormNumber = require('./components/FormNumber');
 const FormStatusWrapper = require('./components/FormStatusWrapper');
 const IfResolved = require('./components/IfResolved');
 const IfFailed = require('./components/IfFailed');
@@ -15,6 +16,7 @@ module.exports = {
   FormField,
   FormCheckbox,
   FormSelect,
+  FormNumber,
   FormButton,
   FormFile,
   FormStatusWrapper,
