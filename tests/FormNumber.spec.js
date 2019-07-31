ad87671ff30acfbb7a86a4a3239805ed975ad365
Paul Wiarda <wiarda@gmail.com>
2019-07-31 23:02:38 +0800

    -snapshot tests for FormField and FormNumber

--- tests/FormNumber.spec.js
+++ tests/FormNumber.spec.js
@@ -1,24 +1,24 @@
 /* global describe test expect */
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { FormField } from '../index';
+import { FormNumber } from '../index';
 
-describe('Field component', () => {
+describe('Number component', () => {
   test('Matches snapshot', () => {
-    const component = renderer.create(<FormField name="email" />).toJSON();
+    const component = renderer.create(<FormNumber name="pets" />).toJSON();
     expect(component).toMatchSnapshot();
   });
 
   test('Renders required props', () => {
     const component = renderer
-      .create(<FormField name="test" label="This is a test field" />)
+      .create(<FormNumber name="test" label="This is a test field" />)
       .toJSON();
     expect(component).toMatchSnapshot();
   });
 
   test('Renders required field', () => {
     const component = renderer
-      .create(<FormField name="test" label="This is a test field" required />)
+      .create(<FormNumber name="test" label="This is a test field" required />)
       .toJSON();
     expect(component).toMatchSnapshot();
   });
@@ -26,7 +26,7 @@ describe('Field component', () => {
   test('Renders without a validation message', () => {
     const component = renderer
       .create(
-        <FormField name="test" label="This is a test field" validationMessage={false} required />,
+        <FormNumber name="test" label="This is a test field" validationMessage={false} required />,
       )
       .toJSON();
     expect(component).toMatchSnapshot();
@@ -35,7 +35,7 @@ describe('Field component', () => {
   test('Accepts custom styles', () => {
     const component = renderer
       .create(
-        <FormField
+        <FormNumber
           name="test"
           label="This is a test field"
           className="test-class"
@@ -47,16 +47,35 @@ describe('Field component', () => {
     expect(component).toMatchSnapshot();
   });
 
-  test('Accepts initial value', () => {
+  test('Accepts min and max props', () => {
     const component = renderer
       .create(
-        <FormField
+        <FormNumber
           name="test"
           label="This is a test field"
           className="test-class"
           validationMessage={false}
           required
-          initialValue="start off with this"
+          min={1}
+          max={10}
+        />,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  test('Accepts intitial value', () => {
+    const component = renderer
+      .create(
+        <FormNumber
+          name="test"
+          label="This is a test field"
+          className="test-class"
+          validationMessage={false}
+          required
+          min={1}
+          max={10}
+          intialValue={7}
         />,
       )
       .toJSON();
