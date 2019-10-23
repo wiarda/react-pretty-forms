406f999f08e6751aa83964e1ab7e9a5559825497
Paul Wiarda <wiarda@gmail.com>
2019-10-23 22:45:59 +0800

    -tests for class methods -updated snapshots

--- tests/Checkbox.spec.js
+++ tests/Checkbox.spec.js
@@ -3,7 +3,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { FormCheckbox } from '../index';
 
-describe('Button component', () => {
+describe('Checkbox component', () => {
   test('Matches snapshot', () => {
     const component = renderer.create(<FormCheckbox />).toJSON();
     expect(component).toMatchSnapshot();
@@ -16,15 +16,70 @@ describe('Button component', () => {
 
   test('Accepts custom styles', () => {
     const component = renderer
-      .create(<FormCheckbox type="button" value="Register" className="test-class" />)
+      .create(<FormCheckbox value="Register" className="test-class" />)
       .toJSON();
     expect(component).toMatchSnapshot();
   });
 
   test('Accepts data props', () => {
     const component = renderer
-      .create(<FormCheckbox type="button" value="Register" className="test-class" data-test="test" />)
+      .create(<FormCheckbox value="Register" className="test-class" data-test="test" />)
       .toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  test('getValue method correctly reads checked value', () => {
+    const props = {
+      value: 'Register',
+      className: 'test-class',
+      checked: true,
+    };
+
+    const checked = renderer
+      .create(<FormCheckbox {...props} />, {
+        createNodeMock: element => {
+          if (element.type === 'input') {
+            // mock ref
+            return {
+              checked: true,
+              value: 'Register',
+            };
+          }
+        },
+      })
+      .getInstance();
+
+    const unchecked = renderer
+      .create(<FormCheckbox {...props} />, {
+        createNodeMock: element => {
+          if (element.type === 'input') {
+            // mock ref
+            return {
+              checked: false,
+              value: 'Register',
+            };
+          }
+        },
+      })
+      .getInstance();
+
+    expect(checked.getValue()).toBe('Register');
+    expect(unchecked.getValue()).toBe(false);
+  });
+
+  test('default validator returns expected values', () => {
+    const props = {
+      value: 'Register',
+      className: 'test-class',
+      checked: true,
+    };
+
+    const required = renderer.create(<FormCheckbox {...props} required />).getInstance();
+    const unrequired = renderer.create(<FormCheckbox {...props} />).getInstance();
+
+    expect(required.defaultValidator('value input')).toBe(true);
+    expect(required.defaultValidator()).toBe(false);
+    expect(unrequired.defaultValidator('value input')).toBe(true);
+    expect(unrequired.defaultValidator()).toBe(true);
   });
 });
