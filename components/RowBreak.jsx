b940644b51b50f7a9d65c32b82f1dd416e29b0ce
Paul Wiarda <wiarda@gmail.com>
2019-10-26 22:21:49 +0800

    -added PropTypes

--- components/RowBreak.jsx
+++ components/RowBreak.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable react/require-default-props */
 const React = require('react');
+const PropTypes = require('prop-types');
 
-function RowBreak({ styles = {}, rowClassName = styles.prettyRow || 'pretty-row', children }) {
+function RowBreak({ styles, rowClassName = styles.prettyRow || 'pretty-row', children }) {
   return <div className={rowClassName}>{children}</div>;
 }
 
 RowBreak._prettyType = 'RowBreak';
+
+RowBreak.propTypes = {
+  rowClassName: PropTypes.string,
+  styles: PropTypes.objectOf(PropTypes.string),
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+RowBreak.defaultProps = {
+  styles: {},
+};
 
 module.exports = RowBreak;
