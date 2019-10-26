789dcbb170f2fa903cf85a218793c32873863091
Paul Wiarda <wiarda@gmail.com>
2019-10-26 21:34:05 +0800

    -added method tests

--- tests/Field.spec.js
+++ tests/Field.spec.js
@@ -62,4 +62,76 @@ describe('Field component', () => {
       .toJSON();
     expect(component).toMatchSnapshot();
   });
+
+  const component = renderer
+    .create(
+      <FormField
+        name="test"
+        label="test"
+        className="test-class"
+        validationMessage="A custom validation message"
+        validator={() => false}
+        required
+      />,
+      {
+        createNodeMock: element => {
+          if (element.type === 'input') {
+            return {
+              value: 'I have a value',
+            };
+          }
+          return {};
+        },
+      },
+    )
+    .getInstance();
+
+  test('Click handler resets field state', () => {
+    component.clickHandler();
+    expect.assertions(1);
+    expect(component.state).toEqual({
+      labelUp: true,
+      isValid: true,
+      validationMessage: 'A custom validation message',
+    });
+  });
+
+  test('If specified, getValidationMessage returns custom message', () => {
+    expect.assertions(1);
+    expect(component.getValidationMessage()).toBe('A custom validation message');
+  });
+
+  test('If specified, deriveValidityState uses custom validator', () => {
+    expect.assertions(1);
+    expect(component.deriveValidityState()).toBe(false);
+  });
+
+  test('blurHandler correctly sets state', () => {
+    component.blurHandler();
+    expect.assertions(1);
+    expect(component.state).toEqual({
+      labelUp: true,
+      isValid: false,
+      validationMessage: 'A custom validation message',
+    });
+  });
+
+  test('blurHandler aborts on mobile blur', () => {
+    const mobileBlurComponent = renderer
+      .create(<FormField mobileBlur={false} name="test" label="test" />)
+      .getInstance();
+    mobileBlurComponent.isMobile = true;
+    expect.assertions(1);
+    expect(mobileBlurComponent.blurHandler()).toBe(undefined);
+  });
+
+  test('focusHandler sets label up', () => {
+    expect.assertions(1);
+    component.focusHandler();
+    expect(component.state).toEqual({
+      labelUp: true,
+      isValid: true,
+      validationMessage: 'A custom validation message',
+    });
+  });
 });
